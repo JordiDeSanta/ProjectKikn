2694ef038a5d02989964a51015ce041f6c405123
Jordi <jordi.bravo.bad@gmail.com>
2021-07-12 19:05:32 -0400

    Initialize Project

---
+++ Source/ProjectKik/ProjectKikGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectKikGameMode.h"
+#include "ProjectKikCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AProjectKikGameMode::AProjectKikGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/ProjectKik/ProjectKikGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectKikGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectKikGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectKikGameMode();
+};
+
+
+
